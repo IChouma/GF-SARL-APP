8a0894eef7c9f3c96f7299faa75ce05de045cd80
Ichouma <abdeljabarichouma@gmail.com>
2022-07-24 20:20:27 +0100

    gf-sarl

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
+import { useRef } from 'react';
 import styles from './App.css';
 import urls from "./data"
 
 function App() {
   const state=urls;
- const layout=state.map((url)=>{
+  console.log(state[0]);
+  function loadimages(numImage=urls.length){
 
+    let i=0;
+    while(i<numImage){
+  const section=document.getElementById('layout')
+   const div=document.createElement('div')
+   const img=document.createElement('img')
+   const h5=document.createElement('h5')
+   h5.classList.add('h5')
+   div.classList.add('div')
+   img.src=`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${state[i]}.JPG`;
+   div.appendChild(img)
+   div.appendChild(h5)
+   const urlt=document.createTextNode(`_${state[i]}`)
+   h5.appendChild(urlt)
+   document.body.appendChild(div)
+   i++
+    }
+  }
   return (
-    <div key={Math.random()} className="layout">
-      <img src={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${url}.JPG`} alt="" />
-      <h5>_{url}.jpg</h5>
-    </div>
-  )
- })
-  return (
-  <section id='layout'>
-{layout}
-  </section>
+    <>
+    {loadimages()}
+{window.addEventListener('scroll',()=>{
+  if(window.scrollY + window.innerHeight>=document.documentElement.scrollHeight){
+loadimages()
+  }
+})
+ 
+}
+    </>
   );
 }
 
